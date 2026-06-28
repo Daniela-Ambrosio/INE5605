4edf534564a79df93db848cf6a72e28e8338bde7
Daniela <daniovv@gmail.com>
2026-06-28 14:35:21 -0300

    separação de controllers da classe Pessoa

--- controllers/controlador_sistema.py
+++ controllers/controlador_sistema.py
@@ -1,6 +1,7 @@
 from views.menu_view import MenuView
 from controllers.clinica_controller import ClinicaController
-from controllers.pessoa_controller import PessoaController
+from controllers.paciente_controller import PacienteController
+from controllers.profissional_controller import ProfissionalController
 from controllers.atendimento_controller import AtendimentoController
 from controllers.pagamento_controller import PagamentoController
 from controllers.relatorios_controller import RelatoriosController
@@ -11,7 +12,7 @@ class ControladorSistema:
         self.__context = Context()
         self.__menu_view = MenuView()
         self.__clinica_controller = ClinicaController(self.context)
-        self.__pessoa_controller = PessoaController(self.context)
+        self.__paciente_controller = PacienteController(self.context)
         self.__atendimento_controller = AtendimentoController(self.context)
         self.__pagamento_controller = PagamentoController(self.context)
         self.__relatorios_controller = RelatoriosController(self.context)
@@ -24,9 +25,9 @@ class ControladorSistema:
             elif opcao == 1:
                 self.__clinica_controller.abrir_tela()
             elif opcao == 2:
-                self.__pessoa_controller.abrir_tela_paciente()
+                self.__paciente_controller.abrir_tela_paciente()
             elif opcao == 3:
-                self.__pessoa_controller.abrir_tela_profissional()
+                self.__profissional_controller.abrir_tela_profissional()
             elif opcao == 4:
                 self.__atendimento_controller.abrir_tela_atendimento()
             elif opcao == 5:
