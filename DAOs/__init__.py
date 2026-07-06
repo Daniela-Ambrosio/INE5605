316983fcee222fe9d825bf0b813dc7cad7441f44
Daniela <daniovv@gmail.com>
2026-07-06 13:31:04 -0300

    refatoração da classe DAO para sincronizar as instâncias em memória

---
+++ DAOs/__init__.py
@@ -0,0 +1,6 @@
+from .atendimento_dao import AtendimentoDAO
+from .clinica_dao import ClinicaDAO
+from .paciente_dao import PacienteDAO
+from .profissional_dao import ProfissionalDAO
+from .procedimento_dao import ProcedimentoDAO
+from .pagamento_dao import PagamentoDAO
--- DAOs/dao.py
+++ DAOs/dao.py
@@ -21,29 +21,47 @@ class DAO(ABC):
         self.__cache = pickle.load(open(self.__datasource, "rb"))
 
     def add(self, key, obj):
+        try:
+            self.__load() # Recarrega os dados do disco antes de alterar
+        except FileNotFoundError:
+            pass
         self.__cache[key] = obj
-        self.__dump()  # atualiza o arquivo depois de add um novo objeto
-
+        self.__dump()
+        
     def update(self, key, obj):
         try:
+            self.__load() # Recarrega antes de alterar
+        except FileNotFoundError:
+            pass
+        try:
             if self.__cache[key] != None:
-                self.__cache[key] = obj  # atualiza a entrada
-                self.__dump()  # atualiza o arquivo
+                self.__cache[key] = obj
+                self.__dump()
         except KeyError:
             raise("Chave inválida")
-
     def get(self, key):
+        try:
+            self.__load() # Recarrega antes de ler
+        except FileNotFoundError:
+            pass
         try:
             return self.__cache[key]
         except KeyError:
             raise("Chave inválida")
-
     def remove(self, key):
         try:
+            self.__load() # Recarrega antes de remover
+        except FileNotFoundError:
+            pass
+        try:
             self.__cache.pop(key)
-            self.__dump()  # atualiza o arquivo depois de remover um objeto
+            self.__dump()
         except KeyError:
             raise("Chave inválida")
             
     def get_all(self):
+        try:
+            self.__load() # Recarrega antes de listar
+        except FileNotFoundError:
+            pass
         return self.__cache.values()
